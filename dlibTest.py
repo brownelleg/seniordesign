97cbc9df46f586ce2d728e08451cc84372bffd1a
trippek <97704223+trippek@users.noreply.github.com>
2022-02-16 17:05:54 -0600

    Update dlibTest.py
    Convert to function that can be called in other python script

--- dlibTest.py
+++ dlibTest.py
@@ -1,56 +1,54 @@
 import cv2
 import dlib
 
+class OpticPixels:
+    @staticmethod
+    def getOpticNostrilPixel(opticsFilename: str):
+        # Load the detector
+        detector = dlib.get_frontal_face_detector()
 
+        # Load the predictor
+        predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# Load the detector
-detector = dlib.get_frontal_face_detector()
+        # read the image
+        img = cv2.imread(opticsFilename)
 
-# Load the predictor
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+        # Convert image into grayscale
+        gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
 
-# read the image
-img = cv2.imread("Karis.5MeterOptics.jpg")
+        # Use detector to find landmarks
+        faces = detector(gray)
 
-# Convert image into grayscale
-gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+        for face in faces:
+            x1 = face.left()  # left point
+            y1 = face.top()  # top point
+            x2 = face.right()  # right point
+            y2 = face.bottom()  # bottom point
 
-# Use detector to find landmarks
-faces = detector(gray)
+            dimensions = img.shape
 
-for face in faces:
-    x1 = face.left() # left point
-    y1 = face.top() # top point
-    x2 = face.right() # right point
-    y2 = face.bottom() # bottom point
+            # height, width, number of channels in image
+            height = img.shape[0]
+            width = img.shape[1]
+            channels = img.shape[2]
 
-    dimensions = img.shape
+            # Look for the landmarks
+            landmarks = predictor(image=gray, box=face)
+            x_right = landmarks.part(34).x
+            y_right = landmarks.part(34).y
+            pixelLocations = [x_right, y_right]
 
-    # height, width, number of channels in image
-    height = img.shape[0]
-    width = img.shape[1]
-    channels = img.shape[2]
+            # Draw a circle
+            cv2.circle(img=img, center=(x_right, y_right), radius=5, color=(0, 255, 0), thickness=-1)
 
-    print('Image Dimension    : ', dimensions)
-    print('Image Height       : ', height)
-    print('Image Width        : ', width)
-    print('Number of Channels : ', channels)
+        # show the image
+        cv2.imshow(winname="Face", mat=img)
 
-    # Look for the landmarks
-    landmarks = predictor(image=gray, box=face)
-    x_right = landmarks.part(34).x
-    y_right = landmarks.part(34).y
+        # Wait for a key press to exit
+        #cv2.waitKey(delay=0)
 
-    print('X coordinate: ' + str(x_right) + '\nY coordinate: ' + str(y_right))
+        # Close all windows
+        cv2.destroyAllWindows()
 
-    # Draw a circle
-    cv2.circle(img=img, center=(x_right, y_right), radius=5, color=(0, 255, 0), thickness=-1)
+        return pixelLocations
 
-# show the image
-cv2.imshow(winname="Face", mat=img)
-
-# Wait for a key press to exit
-cv2.waitKey(delay=0)
-
-# Close all windows
-cv2.destroyAllWindows()
