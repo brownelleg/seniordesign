787b175d54a53b35192652cf2d670491c6493f4e
trippek <97704223+trippek@users.noreply.github.com>
2022-04-06 17:28:03 -0500

    Update ThermalOpticalTakePhoto.py

--- ThermalOpticalTakePhoto.py
+++ ThermalOpticalTakePhoto.py
@@ -1,10 +1,7 @@
-import os
 from ThermalCameraTest import OpticToThermal
 from dlibTest import OpticPixels
-import matplotlib.pyplot as plt
 import time
-import numpy as np
-from moving_average import exponential_moving_avereage, moving_average, weighted_moving_average
+
 
 xthermalLocations = []
 xthermalLocations = [0 for i in range(10)]
@@ -15,43 +12,31 @@ values = [0 for i in range(210)]
 frame = []
 frame = [i for i in range(210)]
 
-for j in range(10):
-    tic = time.time()
-    opticsFilename = '/home/pi/libseek-thermal-master/build/examples/image.jpg'
-    os.system("sudo fswebcam " + opticsFilename)
-    [x_optics, y_optics] = OpticPixels.getOpticNostrilPixel(opticsFilename)
-    [x_thermal, y_thermal] = OpticToThermal.optictothermalpixel(x_optics, y_optics)
-    xthermalLocations[j] = x_thermal
-    ythermalLocations[j] = y_thermal
-    toc = time.time()
-    for i in range(21):
-        thermalFilename = "/home/pi/libseek-thermal-master/build/examples/vid_frame" + str(i+1+21*j) + ".png"
-        #print(x_thermal, y_thermal)
-        values[i+21*j] = OpticToThermal.thermalpixelstovalues(x_thermal, y_thermal, thermalFilename)
-        #time.sleep(0.14)
-        #print(OpticToThermal.thermalpixelstovalues(x_thermal, y_thermal, thermalFilename))
-        #print(values[i])
+previousOpticsFilename = '/home/pi/PycharmProjects/seniordesign/im1.png'
 
+for j in range(6):
+    #if (OpticPixels.getOpticNostrilPixel(opticsFilename) == [0, 0]):
+    #    print("No face detected")
+    #    exit(0)
+    opticsFilename = '/home/pi/PycharmProjects/seniordesign/im' + str(j + 1) + '.png'
+    tic = time.time()
+    if j == 0:
+        [x_optics, y_optics] = OpticPixels.getOpticNostrilPixel(opticsFilename)
+        [x_thermal, y_thermal] = OpticToThermal.optictothermalpixel(x_optics, y_optics)
+    else:
+        diff1 = OpticPixels.checkImageDifference(opticsFilename)
+        diff2 = OpticPixels.checkImageDifference(previousOpticsFilename)
+        if abs(diff1 - diff2) > 50:
+            [x_optics, y_optics] = OpticPixels.getOpticNostrilPixel(opticsFilename)
+            [x_thermal, y_thermal] = OpticToThermal.optictothermalpixel(x_optics, y_optics)
+            previousOpticsFilename = opticsFilename
+    OpticToThermal.displaythermalpix(x_thermal, y_thermal, "/home/pi/vid_frame" + str(j * 35 + 1) + ".png")
+    toc = time.time()
+
+    for i in range(35):
+        thermalFilename = "/home/pi/vid_frame" + str(i+1+35*j) + ".png"
+        values[i+35*j] = OpticToThermal.thermalpixelstovalues(x_thermal, y_thermal, thermalFilename)
 
     print(toc-tic)
 
-# check the thermal pixel location after finding thermal pixel location
-#OpticToThermal.displaythermalpix(x_thermal, y_thermal, '/home/pi/libseek-thermal-master/build/examples/vid_frame1.png')
-
-plt.plot(frame,values)
-plt.show()
-
-#t = np.arange(0.0,2.0, 1e-2)
-#y = np.sin(t) + np.random.rand(t.shape[0])
-#n = 6
-
-#new_data_exp = exponential_moving_avereage(values, n)
-#new_data_weight = weighted_moving_average(values, n)
-#new_data = moving_average(values, n)
-
-#plt.plot(values, label = 'Original Pixel Values')
-#plt.plot(new_data_exp, label = 'Exp Filtered data')
-#plt.plot(new_data_weight, label = 'Weight Filtered data')
-#plt.plot(new_data, label = 'Smoothed Pixel Values')
-#plt.legend()
-#plt.show()
+OpticToThermal.showData(values)
