02b2132722cdb8cc8d560732ed7a6b34d2ad36da
trippek <97704223+trippek@users.noreply.github.com>
2022-04-07 14:36:07 -0500

    Update ThermalOpticalTakePhoto.py

--- ThermalOpticalTakePhoto.py
+++ ThermalOpticalTakePhoto.py
@@ -2,17 +2,21 @@ from ThermalCameraTest import OpticToThermal
 from dlibTest import OpticPixels
 import time
 
+# set parameters
+opticTiming = 3             # timing buffer for frames that take ms to process
+totalSecondOfData = 30      # the amount of seconds of data taken
+buffer = 100                # plus/minus buffer is the amount of variance acceptable between optical images so that
+                            # ML algorithm does not have to rerun
+intervalOpticalData = 5     # amount of seconds between each optical image in data taken
 
-xthermalLocations = []
-xthermalLocations = [0 for i in range(10)]
-ythermalLocations = []
-ythermalLocations = [0 for i in range(10)]
-values = []
-values = [0 for i in range(210)]
-frame = []
-frame = [i for i in range(210)]
+# initialize variables
+values = []                 # stores pixel values
+values = [0 for i in range(totalSecondOfData*7)]
+frame = []                  # matrix of 1 to the number of frames for plotting data
+frame = [i for i in range(totalSecondOfData*7)]
+previousOpticsFilename = '/home/pi/PycharmProjects/seniordesign/im1.png'    # set to the first optics image
 
-previousOpticsFilename = '/home/pi/PycharmProjects/seniordesign/im1.png'
+
 
 for j in range(6):
     #if (OpticPixels.getOpticNostrilPixel(opticsFilename) == [0, 0]):
@@ -26,17 +30,23 @@ for j in range(6):
     else:
         diff1 = OpticPixels.checkImageDifference(opticsFilename)
         diff2 = OpticPixels.checkImageDifference(previousOpticsFilename)
-        if abs(diff1 - diff2) > 50:
+        if abs(diff1 - diff2) > buffer:
             [x_optics, y_optics] = OpticPixels.getOpticNostrilPixel(opticsFilename)
             [x_thermal, y_thermal] = OpticToThermal.optictothermalpixel(x_optics, y_optics)
             previousOpticsFilename = opticsFilename
-    OpticToThermal.displaythermalpix(x_thermal, y_thermal, "/home/pi/vid_frame" + str(j * 35 + 1) + ".png")
+    #OpticToThermal.displaythermalpix(x_thermal, y_thermal, "/home/pi/vid_frame" + str(j * 35 + 1) + ".png")
+
+
+    for i in range(intervalOpticalData*7):
+        thermalFilename = "/home/pi/vid_frame" + str(i+1+intervalOpticalData*7*j) + ".png"
+        values[i+intervalOpticalData*7*j] = OpticToThermal.thermalpixelstovalues(x_thermal, y_thermal, thermalFilename)
+
     toc = time.time()
-
-    for i in range(35):
-        thermalFilename = "/home/pi/vid_frame" + str(i+1+35*j) + ".png"
-        values[i+35*j] = OpticToThermal.thermalpixelstovalues(x_thermal, y_thermal, thermalFilename)
-
-    print(toc-tic)
+    delay = toc - tic
+    print('first delay: ' + str(delay))
+    if delay < opticTiming:
+        time.sleep(opticTiming-delay)
+    toc2 = time.time()
+    print('total: ' + str(toc2 - tic))
 
 OpticToThermal.showData(values)
