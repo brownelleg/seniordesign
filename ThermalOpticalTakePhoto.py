31c530aeefd9d9d2a77bde576f83a37b99f85544
trippek <97704223+trippek@users.noreply.github.com>
2022-04-18 17:13:07 -0500

    Update ThermalOpticalTakePhoto.py
    Live time feature added

--- ThermalOpticalTakePhoto.py
+++ ThermalOpticalTakePhoto.py
@@ -3,30 +3,37 @@ from dlibTest import OpticPixels
 import time
 
 # set parameters
-opticTiming = 3             # timing buffer for frames that take ms to process
+maxFrames = 211             # total number of seconds * 7
+opticTiming = 1             # timing buffer for frames that take ms to process
 totalSecondOfData = 30      # the amount of seconds of data taken
+# intervalOpticalData = 5     # amount of seconds between each optical image in data taken
 buffer = 100                # plus/minus buffer is the amount of variance acceptable between optical images so that
                             # ML algorithm does not have to rerun
-intervalOpticalData = 5     # amount of seconds between each optical image in data taken
+thermalPath = "/home/pi/Thermal_Images/vid_frame"
+opticalPath = "/home/pi/PycharmProjects/seniordesign/image"
+
 
 # initialize variables
 values = []                 # stores pixel values
-values = [0 for i in range(totalSecondOfData*7)]
+values = [0 for i in range(maxFrames)]
 frame = []                  # matrix of 1 to the number of frames for plotting data
-frame = [i for i in range(totalSecondOfData*7)]
-previousOpticsFilename = '/home/pi/PycharmProjects/seniordesign/im1.png'    # set to the first optics image
+frame = [i for i in range(maxFrames)]
 
+firstThermal = 0
+nextThermal = 1
+j = 0
 
-
-for j in range(6):
-    #if (OpticPixels.getOpticNostrilPixel(opticsFilename) == [0, 0]):
+while (nextThermal < maxFrames):
+    #while (OpticPixels.getOpticNostrilPixel(opticsFilename) == [0, 0]):
     #    print("No face detected")
     #    exit(0)
-    opticsFilename = '/home/pi/PycharmProjects/seniordesign/im' + str(j + 1) + '.png'
     tic = time.time()
+    OpticPixels.takeSingleOpticImage(j)
+    opticsFilename = opticalPath + str(j) + ".png"
     if j == 0:
         [x_optics, y_optics] = OpticPixels.getOpticNostrilPixel(opticsFilename)
         [x_thermal, y_thermal] = OpticToThermal.optictothermalpixel(x_optics, y_optics)
+        previousOpticsFilename = opticsFilename
     else:
         diff1 = OpticPixels.checkImageDifference(opticsFilename)
         diff2 = OpticPixels.checkImageDifference(previousOpticsFilename)
@@ -34,12 +41,7 @@ for j in range(6):
             [x_optics, y_optics] = OpticPixels.getOpticNostrilPixel(opticsFilename)
             [x_thermal, y_thermal] = OpticToThermal.optictothermalpixel(x_optics, y_optics)
             previousOpticsFilename = opticsFilename
-    #OpticToThermal.displaythermalpix(x_thermal, y_thermal, "/home/pi/vid_frame" + str(j * 35 + 1) + ".png")
-
-
-    for i in range(intervalOpticalData*7):
-        thermalFilename = "/home/pi/vid_frame" + str(i+1+intervalOpticalData*7*j) + ".png"
-        values[i+intervalOpticalData*7*j] = OpticToThermal.thermalpixelstovalues(x_thermal, y_thermal, thermalFilename)
+    OpticToThermal.displaythermalpix(x_thermal, y_thermal, thermalPath + str(nextThermal - 1) + ".png")
 
     toc = time.time()
     delay = toc - tic
@@ -48,5 +50,14 @@ for j in range(6):
         time.sleep(opticTiming-delay)
     toc2 = time.time()
     print('total: ' + str(toc2 - tic))
+
+    nextThermal = OpticToThermal.MostRecentFrame() + 1
+
+    for i in range(firstThermal, nextThermal):
+        thermalFilename = thermalPath + str(i) + ".png"
+        values[i] = OpticToThermal.thermalpixelstovalues(x_thermal, y_thermal, thermalFilename)
+
+    firstThermal = nextThermal
+
 
 OpticToThermal.showData(values)
